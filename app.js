885462a62335e9254d0a51513b79270289753af7
akash4320 <akash4320@gmail.com>
2023-10-04 01:24:10 +0530

    Allow CORS: Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const cors = require("cors")
 
 const cartRouter = require("./routes/CartRoutes");
 
@@ -10,6 +11,7 @@ dotenv.config();
 
 //middleware
 app.use(express.json());
+app.use(cors())
 app.use("/api/cart", cartRouter);
 
 // MongoDB Connect
